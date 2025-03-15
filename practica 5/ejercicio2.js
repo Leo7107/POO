db8cc9c590af3ea371a1fec55b62178d43ff81f2
Leo7107 <eduardo710722@gmail.com>
2025-03-14 21:07:49 -0600

    Leo

---
+++ practica 5/ejercicio2.js
@@ -0,0 +1,17 @@
+class Padre {
+    constructor(nombre, edad){
+        this._nombre = nombre;
+        this._edad = edad;
+    }
+
+    saludar(){
+        console.log(`Hola! Mi nombre es ${this._nombre} y tengo ${this._edad} años.`);
+    }
+}
+class Hija extends Padre{
+    constructor(nombre, edad){
+        super(nombre, edad);
+    }
+}
+const hija = new Hija("Estrella", 15);
+hija.saludar();
